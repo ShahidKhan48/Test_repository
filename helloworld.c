8e7e1d5f1b0c6dadc5e2d2386a8ca0727eb5025d
shahid <md7481920@gmail.com>
2023-07-21 15:36:02 +0530

    code addition

---
+++ helloworld.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("hello world\n");
+
+    printf("hello Mr. Shahid\n");
+    printf("App kaisi ho meri jaan\n");
+    
+    return 0;
+}
